38e43ff4d62a4a95e058aac9a67c112e1c9dbea7
hakatanaoto <hakatanaoto@localhost>
2026-02-24 22:45:54 +0900

    curriculum_k 修正

--- HelloWorld_HakataNaoto/src/curriculum_k/Main.java
+++ HelloWorld_HakataNaoto/src/curriculum_k/Main.java
@@ -1,16 +1,14 @@
 package curriculum_k;
 
 public class Main {
-	public static void main(String[] args) {
-		SalaryReporter reporter = new SalaryReporter();
+    public static void main(String[] args) {
+        SalaryReporter reporter = new SalaryReporter();
 
-		Employee fullTime = new Employee("001", 160, new FullTimeSalaryStrategy());
-		reporter.report(fullTime);  
+        Employee fullTime = new Employee("001", "田中太郎", 160, new FullTimeSalaryStrategy());
+        reporter.report(fullTime);  
 
-		Employee contract = new Employee("002", 160, new ContractSalaryStrategy());
-		reporter.report(contract);  
-	}
-
-
-
+        Employee contract = new Employee("002", "鈴木花子", 160, new ContractSalaryStrategy());
+        reporter.report(contract);  
+        
+    }
 }
