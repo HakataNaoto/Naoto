0836301b76936a2ec6f53fd6e6880e93a7950db3
hakatanaoto <hakatanaoto@localhost>
2026-02-24 22:46:31 +0900

    curriculum_k 修正

--- HelloWorld_HakataNaoto/src/curriculum_k/Employee.java
+++ HelloWorld_HakataNaoto/src/curriculum_k/Employee.java
@@ -1,55 +1,50 @@
 package curriculum_k;
 
-
 interface SalaryStrategy {
- long calculateSalary(int hours);
+	long calculateSalary(int hours);
 }
-
 
 class FullTimeSalaryStrategy implements SalaryStrategy {
- public long calculateSalary(int hours) {
-     return hours * 1200L;
- }
+	public long calculateSalary(int hours) {
+		return hours * 1200L;
+	}
 }
-
 
 class ContractSalaryStrategy implements SalaryStrategy {
- @Override
- public long calculateSalary(int hours) {
-     return hours * 1000L;
- }
+	@Override
+	public long calculateSalary(int hours) {
+		return hours * 1000L;
+	}
 }
-
 
 class Employee {
- private final String employeeId;
- private final String name;
- private final int hours;
- private final SalaryStrategy strategy;
+	private final String employeeId;
+	private final String name;
+	private final int hours;
+	private final SalaryStrategy strategy;
 
- public Employee(String employeeId, String name, int hours, SalaryStrategy strategy) {
-     this.employeeId = employeeId;
-     this.name = name;
-     this.hours = hours;
-     this.strategy = strategy;
- }
+	public Employee(String employeeId, String name, int hours, SalaryStrategy strategy) {
+		this.employeeId = employeeId;
+		this.name = name;
+		this.hours = hours;
+		this.strategy = strategy;
+	}
 
- public String getEmployeeId() {
-     return employeeId;
- }
+	public String getEmployeeId() {
+		return employeeId;
+	}
 
- public String getName() {
-     return name;
- }
+	public String getName() {
+		return name;
+	}
 
- public long getSalary() {
-     return strategy.calculateSalary(hours);
- }
+	public long getSalary() {
+		return strategy.calculateSalary(hours);
+	}
 }
 
-
 class SalaryReporter {
- public void report(Employee e) {
-     System.out.println(e.getName() + " の給与は " + e.getSalary() + " 円です。");
- }
+	public void report(Employee e) {
+		System.out.println(e.getName() + " の給与は " + e.getSalary() + " 円です。");
+	}
 }
