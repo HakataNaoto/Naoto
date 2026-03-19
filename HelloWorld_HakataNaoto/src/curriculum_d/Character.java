e2ac93f65af61dfd5ac72137bc9378566d21f25e
hakatanaoto <hakatanaoto@localhost>
2026-03-20 00:11:40 +0900

    java基礎問題６　提出

--- HelloWorld_HakataNaoto/src/curriculum_d/Character.java
+++ HelloWorld_HakataNaoto/src/curriculum_d/Character.java
@@ -1,10 +1,56 @@
 package curriculum_d;
 
+import java.util.Random;
+
 public class Character {
+	private String name;
+	private int hp;
+	private int at;
+	private int sp;
 
-	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
+	private static Random rand = new Random();
 
+	public Character(String name) {
+		this.name = name;
+		this.hp = rand.nextInt(51) + 50;
+		this.at = rand.nextInt(11) + 5;
+		this.sp = rand.nextInt(11) + 1;
+	}
+
+	public Character(String name, int hp, int at, int sp) {
+		this.name = name;
+		this.hp = hp;
+		this.at = at;
+		this.sp = sp;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public int getHp() {
+		return hp;
+	}
+
+	public int getAt() {
+		return at;
+	}
+
+	public int getSp() {
+		return sp;
+	}
+
+	public boolean isAlive() {
+		return hp > 0;
+	}
+
+	public void attack(Character target) {
+		target.hp -= this.at;
+		if (target.hp < 0) {
+			target.hp = 0;
+		}
+		System.out.println(this.name + " の攻撃！ " + this.at + " のダメージ！");
+		System.out.println(target.name + " の残りHP: " + target.hp);
 	}
 
 }
